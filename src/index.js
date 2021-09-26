b47c322b5bd0e0c1292e9ab8ab849008ee2d07d5
francineMadoh <francinemadoh@gmail.com>
2021-09-26 12:22:22 +0100

    modified some css classes, create a dev branch

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 
 app.get('/', (req,res)=>{
-    res.send('Our awesome magazine')
+    res.render('index')
 })
 
 app.listen(port, ()=>{
