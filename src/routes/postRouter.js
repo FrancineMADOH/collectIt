f4d5f0dfe7f8745ffcf00b65b9d187e2fdf1a14e
francinemadoh@gmail.com <francinemadoh@gmail.com>
2021-10-09 09:31:47 +0100

    feature setup the user signup route

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -4,7 +4,23 @@ const  Posts = require('../models/postsModel')
 
 //setting up routes for users
 router.get('/',(req,res)=>{
-    res.send('hello post')
+    //res.send('hello post')
+    res.render('my_posts')
+})
+
+//get to the new post page 
+router.get('/new', (req,res)=>{
+    res.render('new_post')
+})
+
+// Edit posts
+router.get('/edit', (req,res)=>{
+    res.render('edit_post')
+})
+
+//Delete post
+router.delete('/delete', (req,res)=>{
+
 })
 
 module.exports = router
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const router = new express.Router()
 const Users = require('../models/usersModel')
+const bcrypt = require('bcrypt')
 
-//creating users
-router.post('/',(req,res)=>{
+router.use(express.urlencoded({extended:false}))
+
+//creating users + user sign_up route
+router.post('/sign_up', async (req,res)=>{
     const user = new Users(req.body)
-    user.save().then(()=>{
-        res.send(user)
-    }).catch((e)=>{
-        res.status(400).send(e)
-    })
+    try{
+        //const password = bc
+        await user.save({
+            name: req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            //confirmpw:req.body.confirmpw
+        })
+        //res.status(201).send(user)
+        res.redirect('sign_in')
+        console.log(user)
+    }catch(e){
+        //res.status(400).send(e)
+        res.redirect('sign_up')
+    }
 })
 // signup route
-router.get('/sign_up', (re,res)=>{
+router.get('/sign_up', async (req,res)=>{
+    // try{
+    //     const users = await Users.find({})
+    // }catch(e){
+    //     res.status(500).send()
+    // }
     res.render('sign_up')
 })
 
@@ -26,9 +44,23 @@ router.get('/reset_password', (req, res)=>{
     res.render('reset_password')
 })
 
-//creatting users with post request
+//creating users with post request
 router.post('sign_up', (req,res)=>{
     
+})
+
+//reading a single user
+router.get('/:id', async (req,res)=>{
+    const _id = req.params.id
+    try{
+        const user = await Users.findById(_id)
+        //the user might not exist
+        if(!user){
+            res.status(404).send()
+        }
+    }catch(e){
+        res.status(500).send(e)
+    }
 })
 
 module.exports = router
