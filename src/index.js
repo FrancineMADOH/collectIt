e2068710e1f733a2ec213f4c1da5a4e93e274073
FrancineMADOH <francinemadoh@gmail.com>
2022-07-04 10:06:57 +0100

    ajustement contenu front end

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use('/users',usersRouter)
 app.use('/posts',postRouter)
 
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname,'../views/'))
+app.set('views', path.join(__dirname, '../views/'))
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
 
